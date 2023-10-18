106ea546757066cd3b82c99074fdfe3aa523d6ec
Vinayak Srivastava <vinayak.sri@yahoo.com>
2023-10-19 00:18:29 +0530

    Added comments for better understanding

--- introduction.py
+++ introduction.py
@@ -1,4 +1,6 @@
 print ("Hello!")
-print ("My name is Vinayak")
+print ("My name is Vinayak") 
+#taking name as an input
 name = str(input("What is your name? "))
+#greets the user with the given name
 print ("It is nice to meet you", name)
