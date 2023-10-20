1bf00e17775feebb62576b2e4b094c68aec44c83
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2023-10-20 21:44:54 +0530

    chore:format document

--- change_case.py
+++ change_case.py
@@ -1,15 +1,19 @@
-s=input("Enter the Sentence for case to be changed: ")
+s = input("Enter the Sentence for case to be changed: ")
+
+
 def swap_case(st):
-  s=""
-  x=len(st)
-  for i in range(x):
-    y=st[i]
-    if(y.isupper()):
-      s=s+y.lower()
-    elif(y.islower()):
-      s=s+y.upper()
-    else:
-      s=s+y
-  return s
-result=swap_case(s)
+    s = ""
+    x = len(st)
+    for i in range(x):
+        y = st[i]
+        if y.isupper():
+            s = s + y.lower()
+        elif y.islower():
+            s = s + y.upper()
+        else:
+            s = s + y
+    return s
+
+
+result = swap_case(s)
 print(result)
