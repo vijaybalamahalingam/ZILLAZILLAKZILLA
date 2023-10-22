b2056d4d98330b930e88ad5c2d39df4ac27f5fad
Vinayak Srivastava <vinayak.sri@yahoo.com>
2023-10-22 12:15:19 +0530

    feat: Add python code

--- introduction.py
+++ introduction.py
@@ -1,6 +1,5 @@
-print ("Hello!")
-print ("My name is Vinayak") 
-#taking name as an input
-name = str(input("What is your name? "))
-#greets the user with the given name
-print ("It is nice to meet you", name)
+print ("Hi, my name is Vinayak")
+#taking user input
+n = str(input("What is your name? "))
+#printing the output
+print ("It is nice to meet you ",n)
