895a46c6f34bf1500072b807d7695193af472789
Vinayak Srivastava <vinayak.sri@yahoo.com>
2023-10-24 23:14:27 +0530

    Format introduction.py with black

--- introduction.py
+++ introduction.py
@@ -1,5 +1,5 @@
-print ("Hi, my name is Vinayak")
-#taking user input
+print("Hi, my name is Vinayak")
+# taking user input
 n = str(input("What is your name? "))
-#printing the output
-print ("It is nice to meet you ",n)
+# printing the output
+print("It is nice to meet you ", n)
